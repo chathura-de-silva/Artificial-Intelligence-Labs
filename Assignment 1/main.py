edb64596b855ca91f1491dc004ac807d24778dc9
chathura-de-silva <ac.chathura@gmail.com>
2023-09-29 15:47:15 +0530

    Exception handling improved.

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -40,45 +40,46 @@ try:
     with open("input.txt", "r") as input_file:  # Reading the input file
         input_data = [line.strip() for line in input_file.readlines()]
         # print("input_data  :", input_data) # Uncomment for debugging purposes
+
+    n = len(input_data[0].split(","))  # Number of destinations including starting point
+    graph = []
+
+    for i in range(n):
+        line = input_data[0]  # Reading the first line of the input file
+        graph.append([int(x) if x.isnumeric() else float('inf') for x in line.split(",")])
+        input_data.pop(0)  # Removing the first line of the input file
+
+    # print("graph :", graph) # Uncomment for debugging purposes
+    trucks = []
+    for truck_data in input_data:
+        trucks.append([int(x) if x.isnumeric() else x for x in truck_data.split("#")])
+    # print("Trucks :", trucks) # Uncomment for debugging purposes
+
+    destination_list = [x for x in range(1, n)]  # List of destinations excluding starting point
+    best_solution = []
+    best_distance = float("inf")
+    for i in range(ITERATIONS):
+        current_solution = destination_divider(destination_list, trucks)  # Randomly dividing the destinations into
+        # trucks
+        total_distance = 0
+        for truck in current_solution:
+            total_distance += distance_counter(truck[-1])
+        if total_distance < best_distance:  # If the current solution is better than the best solution, update the best
+            # solution
+            best_distance = total_distance
+            best_solution = current_solution
+            # print("solution :", [x[-1] for x in current_solution], "distance : ", total_distance)
+            # Uncomment above line for debugging purposes
+    for truck in best_solution:
+        truck[-1] = [chr(x + 97) for x in truck[-1]]  # Converting the destinations to their corresponding letters
+    # print("Final answer :", best_solution, best_distance) # Uncomment for debugging purposes
+
+    try:
+        with open(f"{INDEX_NUMBER}.txt", "w") as output_file:  # Writing the output to the file
+            # Below line writes the output in the required format.
+            output_file.writelines(truck[0] + "#" + (",".join(truck[-1])) + "\n" for truck in best_solution)
+            output_file.write(str(best_distance))
+    except IOError:
+        print(f"Error occurred while writing to the {INDEX_NUMBER}.txt!")
 except IOError:
     print("Error occurred while reading the input.txt!")
-
-n = len(input_data[0].split(","))  # Number of destinations including starting point
-graph = []
-
-for i in range(n):
-    line = input_data[0]  # Reading the first line of the input file
-    graph.append([int(x) if x.isnumeric() else float('inf') for x in line.split(",")])
-    input_data.pop(0)  # Removing the first line of the input file
-
-# print("graph :", graph) # Uncomment for debugging purposes
-trucks = []
-for truck_data in input_data:
-    trucks.append([int(x) if x.isnumeric() else x for x in truck_data.split("#")])
-# print("Trucks :", trucks) # Uncomment for debugging purposes
-
-destination_list = [x for x in range(1, n)]  # List of destinations excluding starting point
-best_solution = []
-best_distance = float("inf")
-for i in range(ITERATIONS):
-    current_solution = destination_divider(destination_list, trucks)  # Randomly dividing the destinations into trucks
-    total_distance = 0
-    for truck in current_solution:
-        total_distance += distance_counter(truck[-1])
-    if total_distance < best_distance:  # If the current solution is better than the best solution, update the best
-        # solution
-        best_distance = total_distance
-        best_solution = current_solution
-        # print("solution :", [x[-1] for x in current_solution], "distance : ", total_distance)
-        # Uncomment above line for debugging purposes
-for truck in best_solution:
-    truck[-1] = [chr(x + 97) for x in truck[-1]]  # Converting the destinations to their corresponding letters
-# print("Final answer :", best_solution, best_distance) # Uncomment for debugging purposes
-
-try:
-    with open(f"{INDEX_NUMBER}.txt", "w") as output_file:  # Writing the output to the file
-        # Below line writes the output in the required format.
-        output_file.writelines(truck[0] + "#" + (",".join(truck[-1])) + "\n" for truck in best_solution)
-        output_file.write(str(best_distance))
-except IOError:
-    print(f"Error occurred while writing to the {INDEX_NUMBER}.txt!")
