31915c6733d954fe0e437e7e9c9e5b922de5ff2a
chathura-de-silva <ac.chathura@gmail.com>
2023-10-18 23:05:02 +0530

    initial commit 2

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,24 +1,44 @@
 def get_state_utilities(epsilon, rewards):
-    utilities = [0] * len(rewards)
+    utilities = [0] * 6
     delta = epsilon + 1
+    
+    def get_state(action,current_state):
+        if action == "up":
+            if current_state >= 3:
+                return current_state
+            else:
+                return current_state + 3
+        elif action == "down":
+            if current_state <= 2:
+                return current_state
+            else:
+                return current_state - 3
+        elif action == "left":
+            if current_state % 3 == 0:
+                return current_state
+            else:
+                return current_state - 1
+        elif action == "right":
+            if current_state % 3 == 2:
+                return current_state
+            else:
+                return current_state + 1
+
+    def q_star(current_state,action):
+        left_state = get_state("left", current_state)
+        right_state = get_state("right", current_state)
+        up_state = get_state("up", current_state)
+        down_state = get_state("down", current_state)
+        return (0.05 * (rewards[left_state] + old_utilities[left_state]) + 0.05 * (rewards[down_state] + old_utilities[down_state])
+                + 0.9 * (rewards[up_state] + old_utilities[up_state]))
+        
     while delta > epsilon:
-        delta = 0
-        for state in range(len(rewards)):
+        delta = 0 #check whethet this line is needed
+        for current_state in range(len(rewards)):
             old_utilities = utilities[:]
-            utilities[state] = max(
-                
+            
+            utilities[current_state] = max(
+                q_star(current_state,"left")
             )
     return utilities
 
-
-
-# def get_state_utilities(epsilon, rewards):
-#     utilities = [0] * len(rewards)
-#     delta = epsilon + 1
-#     while delta > epsilon:
-#         delta = 0
-#         for i in range(len(rewards)):
-#             old_utility = utilities[i]
-#             utilities[i] = rewards[i] + max(utilities[i-1], utilities[i+1])
-#             delta = max(delta, abs(old_utility - utilities[i]))
-#     return utilities
