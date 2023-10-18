2afffd9540360c77076f56b8071da2fd05f13da0
chathura-de-silva <ac.chathura@gmail.com>
2023-10-18 23:52:11 +0530

    have to fix a minor bug

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -2,43 +2,49 @@ def get_state_utilities(epsilon, rewards):
     utilities = [0] * 6
     delta = epsilon + 1
     
-    def get_state(action,current_state):
-        if action == "up":
-            if current_state >= 3:
-                return current_state
-            else:
-                return current_state + 3
-        elif action == "down":
-            if current_state <= 2:
-                return current_state
-            else:
-                return current_state - 3
-        elif action == "left":
-            if current_state % 3 == 0:
-                return current_state
-            else:
-                return current_state - 1
-        elif action == "right":
-            if current_state % 3 == 2:
-                return current_state
-            else:
-                return current_state + 1
-
     def q_star(current_state,action):
-        left_state = get_state("left", current_state)
-        right_state = get_state("right", current_state)
-        up_state = get_state("up", current_state)
-        down_state = get_state("down", current_state)
-        return (0.05 * (rewards[left_state] + old_utilities[left_state]) + 0.05 * (rewards[down_state] + old_utilities[down_state])
-                + 0.9 * (rewards[up_state] + old_utilities[up_state]))
         
+        def get_state(action,current_state):
+            if action == 0:
+                if current_state >= 3:
+                    return current_state
+                else:
+                    return current_state + 3
+            elif action == 2:
+                if current_state <= 2:
+                    return current_state
+                else:
+                    return current_state - 3
+            elif action == 3:
+                if current_state % 3 == 0:
+                    return current_state
+                else:
+                    return current_state - 1
+            elif action == 1:
+                if current_state % 3 == 2:
+                    return current_state
+                else:
+                    return current_state + 1
+            
+        if action > 3 or action < 0:
+            if action == 4:
+                return rewards[current_state] + old_utilities[current_state]
+            else:    
+                raise ValueError("Action must be between 0 and 4")
+        
+        side_state_1 = get_state(action-1 if action != 0 else 3, current_state)
+        desired_state  = get_state(action, current_state)
+        side_state_2 = get_state(action+1 if action != 3 else 0 , current_state)
+        q_star_value = 0.05 * (rewards[side_state_1] + old_utilities[side_state_1]) + 0.05 * (rewards[side_state_2] + old_utilities[side_state_2]) + 0.9 * (rewards[desired_state] + old_utilities[desired_state])
+        return q_star_value
+   
     while delta > epsilon:
         delta = 0 #check whethet this line is needed
+        old_utilities = utilities[:]
         for current_state in range(len(rewards)):
-            old_utilities = utilities[:]
-            
-            utilities[current_state] = max(
-                q_star(current_state,"left")
-            )
+            utilities[current_state] = max(q_star(current_state,0), q_star(current_state,1), q_star(current_state,2), q_star(current_state,3), q_star(current_state,4))
+            delta = max(delta, abs(old_utilities[current_state] - utilities[current_state]))
+        print(utilities,delta)
     return utilities
 
+print(get_state_utilities(1, [-0.1, -0.1, 1, -0.1, -0.1, -0.05]))
